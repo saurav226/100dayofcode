df6847527a22e2015cb4ada2495a6026432ffb99
saurav226 <mr.sauravsingh05@gmail.com>
2025-11-22 14:41:55 +0530

    Add files via upload

---
+++ 100daysofcodeday23.2.c
@@ -0,0 +1,22 @@
+
+//day23.2- 100 days of code
+/*Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ 100daysofcodeday25.2.c
@@ -0,0 +1,20 @@
+
+//day25.2- 100 days of code
+/*Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+*/
+#include <stdio.h>
+
+int main() {
+    for(int i = 5; i >= 1; i--) {
+        for(int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
