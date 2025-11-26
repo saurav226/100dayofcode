3630e59b62455f9e2452ca14f5d575b764c30964
saurav226 <mr.sauravsingh05@gmail.com>
2025-11-26 17:03:44 +0530

    Add files via upload

---
+++ 100daysofcodeday29.1.c
@@ -0,0 +1,17 @@
+
+//day29.1- 100 days of code
+// Find the sum of array elements.
+
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    printf("%d", sum);
+    return 0;
+}
